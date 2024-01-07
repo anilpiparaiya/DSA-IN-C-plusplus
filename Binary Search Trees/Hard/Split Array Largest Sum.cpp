fc04a75cfe0e315c61e006913912d2023916509e
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2024-01-07 23:05:05 +0530

    Create Split Array Largest Sum.cpp

---
+++ Binary Search Trees/Hard/Split Array Largest Sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  public:
+    int splitArray(int arr[] ,int N, int K) {
+        // code here
+        int totalSum =0,mx=INT_MIN;
+        for(int i=0;i<N;i++) {
+            totalSum+=arr[i];
+            mx = max(mx,arr[i]);
+        }
+        
+        int lo=mx,hi=totalSum,ans=INT_MAX;
+        while(lo<=hi) {
+            int mid = lo+(hi-lo)/2;
+            if(isValid(arr,mid,K,N)) {
+                ans=min(ans,mid);
+                hi=mid-1;   // To minimize our answer
+            } else {
+                lo=mid+1;   // To get Valid Range
+            }
+        }
+        return ans;
+    }
+    // to check if requiredSum is possible to get by splitting array in k or less then k subarray
+    bool isValid(int arr[],int requiredSum,int k,int n) {
+        int sum=0,count=1;
+        for(int i=0;i<n;i++) {
+            if(sum+arr[i]<=requiredSum) {
+                sum+=arr[i];
+            } else {
+                sum=arr[i];
+                count++;
+            }
+        }
+        return count<=k;
+    }
+};
